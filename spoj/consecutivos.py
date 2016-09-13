394d1977530f816b344901c88a0f05ae01df62a1
Felipe Pontes <felipemfpontes@gmail.com>
2016-09-13 09:07:09 -0300

    Create consecutivos.py

---
+++ spoj/consecutivos.py
@@ -0,0 +1,19 @@
+# br.spoj.com/problems/CONSEC12/
+
+input()
+
+lc = None
+l = input().split()
+m = 0
+am = 0
+
+for c in l:
+	if c == lc:
+		am += 1
+	else:
+		lc = c
+		am = 1
+	if am > m:
+		m = am
+
+print(m)
