482c9872fe7c58feb253a9be3a5f1c15bce37ef7
Felipe Pontes <felipemfpontes@gmail.com>
2016-09-30 10:47:44 -0300

    Add fatorial.c and potencia.c

---
+++ questoes/fatorial.c
@@ -0,0 +1,20 @@
+/*
+ * Escreva um programa que calcule o fatorial de forma recursiva.
+ */
+
+#include "stdio.h"
+
+int recursive_factorial(int x)
+{
+  if (x == 0)
+  {
+    return 1;
+  }
+
+  return x * recursive_factorial(x - 1);
+}
+
+int main(void) {
+  printf("%d\n", recursive_factorial(6)); // expected 720
+  return 0;
+}
---
+++ questoes/potencia.c
@@ -0,0 +1,25 @@
+/*
+ * Escreva um programa que calcule a potência de forma recursiva.
+ */
+
+#include "stdio.h"
+
+double recursive_pow(double x, int y)
+{
+  if (y == 0)
+	{
+    return 1;
+  }
+
+  if (y == 1)
+  {
+    return x;
+  }
+
+  return x * recursive_pow(x, y - 1);
+}
+
+int main(void) {
+  printf("%lf\n", recursive_pow(2, 2)); // expected 4.00000
+  return 0;
+}
